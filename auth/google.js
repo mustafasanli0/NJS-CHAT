1d74ba4ba65bde0f5a7b99eb6a26de083b825b76
Mehmet Acar <mehmetacar683@gmail.com>
2019-11-07 22:50:06 +0300

    Passport.js Google Auth 1

--- auth/google.js
+++ auth/google.js
@@ -2,3 +2,16 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 
 // models
+const User = require('../models/Users');
+
+// middlewares
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_LOGIN_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_LOGIN_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_LOGIN_CALLBACK_URL
+}, (accessToken, refreshToken, profile, done) => {
+    const data = profile._json;
+    console.log(data);
+}));
+
+module.exports = passport;
