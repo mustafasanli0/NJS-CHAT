c16b2fc037b64040c3b48788ddab873f28944a34
Mehmet Acar <mehmetacar683@gmail.com>
2019-11-12 19:49:05 +0300

    socket authorization

--- src/socketApi.js
+++ src/socketApi.js
@@ -1,12 +1,15 @@
 const socketio = require('socket.io');
+const socketAuthorization = require('../middleware/socketAuthorization');
 const io = socketio();
 
 const socketApi = {
     io
 };
 
+io.use(socketAuthorization);
+
 io.on('connection', (socket) => {
-    console.log('A Good User Logged In');
+    console.log('A Good User Logged In -> ' + socket.request.user.name + " " + socket.request.user.surname);
 });
 
 module.exports = socketApi;
