d11b9aae9dc3a14731605f43522d6b50514c8c97
Mehmet Acar <mehmetacar683@gmail.com>
2019-11-12 20:21:42 +0300

    chat-front-tab

--- public/javascripts/controller/chatController.js
+++ public/javascripts/controller/chatController.js
@@ -1,3 +1,9 @@
 app.controller('chatController', ['$scope',($scope) => {
+    $scope.activeTab = 1;
+
+    $scope.changeTab = tab => {
+        $scope.activeTab = tab;
+    };
+
     const socket = io.connect('http://localhost:3000');
 }]);
